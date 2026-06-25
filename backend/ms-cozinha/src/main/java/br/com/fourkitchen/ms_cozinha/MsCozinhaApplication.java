de74ea4ace93580c5090bf06e462b60eac3bd4b4
Raphael Rodrigues Papa <raphael.papa@foursys.com.br>
2026-06-25 17:25:19 -0300

    preparando ambiente backend

---
+++ backend/ms-cozinha/src/main/java/br/com/fourkitchen/ms_cozinha/MsCozinhaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fourkitchen.ms_cozinha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCozinhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCozinhaApplication.class, args);
+	}
+
+}
