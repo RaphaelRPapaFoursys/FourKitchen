7946c153279fdc151d5fa881cbe5c7a890702ce4
Raphael Rodrigues Papa <raphael.papa@foursys.com.br>
2026-06-30 14:15:25 -0300

    adicionando a estrutura inicial do bff

---
+++ backend/bff-restaurante/src/main/java/br/com/fourkitchen/bff_restaurante/BffRestauranteApplication.java
@@ -0,0 +1,13 @@
+package br.com.fourkitchen.bff_restaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffRestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffRestauranteApplication.class, args);
+	}
+
+}
---
+++ backend/bff-restaurante/src/test/java/br/com/fourkitchen/bff_restaurante/BffRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fourkitchen.bff_restaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffRestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
